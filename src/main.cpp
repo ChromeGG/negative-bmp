15680a73669fb4839eaab5cd8f343fc578c05606
Adam Tkaczyk <a.tkaczyk@grupazero.pl>
2022-06-19 15:52:59 +0200

    Add structs

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,34 @@
 #include <stdlib.h>
 #include <limits.h>
 
+struct FileHeader {
+    short bfType;
+    int bfSize;
+    short bfReserved1;
+    short bfReserved2;
+    int bfOffBits;
+} File;
+
+struct PictureHeader {
+    int biSize;
+    int biWidth;
+    int biHeight;
+    short biPlanes;
+    short biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+} Picture;
+
+struct ColorsRGB {
+    char R;
+    char G;
+    char B;
+} Rgb;
+
 int main(int arc, char *argv[]) {
   FILE *w = fopen("../test.bmp", "wb");
   if (w == nullptr) {
