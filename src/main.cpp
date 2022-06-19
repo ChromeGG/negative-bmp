0be6e831bcd87641b5355ac4732bea141a38fa20
Adam Tkaczyk <a.tkaczyk@grupazero.pl>
2022-06-19 15:50:22 +0200

    Handle file opening

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,13 @@
 #include <limits.h>
 
 int main(int arc, char *argv[]) {
-  return 1
+  FILE *w = fopen("../test.bmp", "wb");
+  if (w == nullptr) {
+      printf("\n\n Can't open the file");
+      return -1;
+  } else {
+      printf("\n\n File w opened!");
+  }
+ 
+  return 1;
 }
